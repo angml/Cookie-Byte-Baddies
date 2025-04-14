62c1dc08c7001ddf0c84b6da6bbca54de4f3bdf9
angml <angminloo05@gmail.com>
2025-04-14 15:16:35 -0400

    uh i tihnk i resolved a merge error?

--- app/src/modules/nav.py
+++ app/src/modules/nav.py
@@ -139,13 +139,9 @@ def SideBarLinks(show_home=False):
 
         # Show World Bank Link and Map Demo Link if the user is a political strategy advisor role.
         if st.session_state["role"] == "Manager":
-<<<<<<< Updated upstream
             ManagerPageNav()
-=======
-            ManagerHomeNav()
             CostsNav()
             SalesNav()
->>>>>>> Stashed changes
 
         # If the user role is usaid worker, show the Api Testing page
         if st.session_state["role"] == "usaid_worker":
