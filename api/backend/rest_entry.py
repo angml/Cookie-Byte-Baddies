c0af55285601609d47549f8727e98b9cbe719a79
angml <angminloo05@gmail.com>
2025-04-16 12:06:28 -0400

    fixed errors from merge

--- api/backend/rest_entry.py
+++ api/backend/rest_entry.py
@@ -7,7 +7,6 @@ from backend.simple.simple_routes import simple_routes
 from backend.employees.employees_routes import employees
 from backend.suppliers.supplier_routes import suppliers
 from backend.menu_items.menuItem_routes import menuItems
-from backend.supply_orders.supplyOrder_routes import supplyOrders
 from backend.cost_routes.cost_routes import costs
 from backend.equipment_routes.equipment_routes import equipment
 from backend.sales_routes.sales_routes import sales
@@ -15,11 +14,6 @@ from backend.sales_routes.sales_routes import sales
 import os 
 from dotenv import load_dotenv
 
-
-<<<<<<<<< Temporary merge branch 1
-=========
-
->>>>>>>>> Temporary merge branch 2
 def create_app():
     app = Flask(__name__)
 
@@ -58,7 +52,6 @@ def create_app():
     app.register_blueprint(menuItems,   url_prefix='/m')
     app.register_blueprint(employees,   url_prefix='/e')
     app.register_blueprint(suppliers,   url_prefix='/s')
-    app.register_blueprint(supplyOrders, url_prefix='/so')
     app.register_blueprint(costs, url_prefix='/costs')
     app.register_blueprint(equipment, url_prefix='/eq')
     app.register_blueprint(sales, url_prefix='/sales')
--- app/src/modules/nav.py
+++ app/src/modules/nav.py
@@ -12,48 +12,6 @@ def HomeNav():
 def AboutPageNav():
     st.sidebar.page_link("pages/30_About.py", label="About", icon="🧠")
 
-
-#### ------------------------ Examples for Role of pol_strat_advisor ------------------------
-def ManagerHomeNav():
-    st.sidebar.page_link(
-        "pages/00_Manager_Home.py", label="Manager Home", icon="👤"
-    )
-
-
-def CostsNav():
-    st.sidebar.page_link(
-        "pages/01_Costs.py", label="Costs", icon="🏦"
-    )
-
-
-def SalesNav():
-    st.sidebar.page_link("pages/02_Sales.py", label="Sales", icon="🗺️")
-
-
-## ------------------------ Examples for Role of usaid_worker ------------------------
-def ApiTestNav():
-    st.sidebar.page_link("pages/12_API_Test.py", label="Test the API", icon="🛜")
-
-
-def PredictionNav():
-    st.sidebar.page_link(
-        "pages/11_Prediction_Worker.py", label="Regression Prediction", icon="📈"
-    )
-
-
-def ClassificationNav():
-    st.sidebar.page_link(
-        "pages/13_Classification.py", label="Classification Demo", icon="🌺"
-    )
-
-
-#### ------------------------ System Admin Role ------------------------
-def AdminPageNav():
-    st.sidebar.page_link("pages/20_Admin_Home.py", label="System Admin", icon="🖥️")
-    st.sidebar.page_link(
-        "pages/21_ML_Model_Mgmt.py", label="ML Model Management", icon="🏢"
-    )
-    
 #### ------------------------ Manager Role ------------------------
 def ManagerPageNav():
     st.sidebar.page_link("pages/00_Manager_Home.py", label="Manager Home", icon="🖥️")
@@ -91,35 +49,34 @@ def SideBarLinks(show_home=False):
     This function handles adding links to the sidebar of the app based upon the logged-in user's role, which was put in the streamlit session_state object when logging in.
     """
     
-    st.markdown("""
-    <style>
-        /* Sidebar container background */
-        [data-testid="stSidebar"] {
-            background: linear-gradient(180deg, #1b0b6e 0%, #4e1489 100%);
-            color: white;
-        }
+#     st.markdown("""
+#     <style>
+#         /* Sidebar container background */
+#         [data-testid="stSidebar"] {
+#             background: linear-gradient(180deg, #1b0b6e 0%, #4e1489 100%);
+#             color: white;
+#         }
 
-        /* Sidebar header and text */
-        [data-testid="stSidebar"] * {
-            color: white !important;
-        }
+#         /* Sidebar header and text */
+#         [data-testid="stSidebar"] * {
+#             color: white !important;
+#         }
 
-        /* Sidebar buttons */
-        [data-testid="stSidebar"] button {
-            background-color: #ba59eb !important;
-            color: white !important;
-            border: none;
-            border-radius: 8px;
-        }
+#         /* Sidebar buttons */
+#         [data-testid="stSidebar"] button {
+#             background-color: #ba59eb !important;
+#             color: white !important;
+#             border: none;
+#             border-radius: 8px;
+#         }
 
-        /* Sidebar page links */
-        [data-testid="stSidebar"] a {
-            color: white !important;
-            text-decoration: none;
-        }
-    </style>
-""", unsafe_allow_html=True)
-
+#         /* Sidebar page links */
+#         [data-testid="stSidebar"] a {
+#             color: white !important;
+#             text-decoration: none;
+#         }
+#     </style>
+# """, unsafe_allow_html=True)
 
 
     # add a logo to the sidebar always
@@ -137,22 +94,6 @@ def SideBarLinks(show_home=False):
     # Show the other page navigators depending on the users' role.
     if st.session_state["authenticated"]:
 
-        # Show World Bank Link and Map Demo Link if the user is a political strategy advisor role.
-        if st.session_state["role"] == "Manager":
-            ManagerPageNav()
-            CostsNav()
-            SalesNav()
-
-        # If the user role is usaid worker, show the Api Testing page
-        if st.session_state["role"] == "usaid_worker":
-            PredictionNav()
-            ApiTestNav()
-            ClassificationNav()
-
-        # If the user is an administrator, give them access to the administrator pages
-        if st.session_state["role"] == "administrator":
-            AdminPageNav()
-            
         # If the user is a customer, give them access to the customer pages (menu)
         if st.session_state["role"] == "customer":
             CustomerHomeNav()
